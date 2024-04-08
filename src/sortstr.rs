d09cb4fbe45805470a04b7059ca5bd6f465f577b
Nicola Da Rold <ndarold@pataviumenergia.it>
2024-04-08 11:31:33 +0200

    add: sortstr

--- src/sortstr.rs
+++ src/sortstr.rs
@@ -1,11 +1,41 @@
 pub fn sort() {
-    get_random_strings();
-    
+    let strings = get_random_strings();
+    print_strings("unsorted: ".into(), &strings);
+    let sorted: Vec<String> = get_sorted_strings(&strings);
+    print_strings("sorted:   ".into(), &sorted);
 }
 
-fn get_random_strings() {// -> Vec<&'static str> {
-    let strings = vec!["Mick", "Craig", "Wayne", "Simon"];
-    dbg!(strings);
-    // return strings;
+fn get_sorted_strings(unsorted: &Vec<String>) -> Vec<String> {
+    let mut sorted = vec![];
+    // copy borrowed array in to-be-sorted array
+    for u in unsorted {
+        sorted.push(u.clone());
+    }
+    // apply bubble sort
+    let mut keep_cycling: bool = true;
+    while keep_cycling {
+        keep_cycling = false;
+        for i in 0..sorted.len()-1 {
+            if sorted[i] > sorted[i+1] {
+                let tmp = sorted[i].clone();
+                sorted[i] = sorted[i+1].clone();
+                sorted[i+1] = tmp;
+                keep_cycling = true;
+            }
+        }
+    }
+    return sorted;
+}
+
+fn get_random_strings() -> Vec<String> {
+    return vec!["Wayne".into(), "Craig".into(), "Mick".into(), "Simon".into()];
+}
+
+fn print_strings(caption: String, strings: &Vec<String>) {
+    print!("{}", caption);
+    for s in strings {
+        print!("{} ", s);
+    }
+    println!("");
 }
 
